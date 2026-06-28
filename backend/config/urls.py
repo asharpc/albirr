74e398fef7a07755db114a3d68863e8875ea2623
Ashar <158561839+asar-eduport@users.noreply.github.com>
2026-06-28 10:57:02 +0530

    Redirect / to /swagger/ so bare-IP visits land on API docs

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -36,6 +37,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/swagger/', permanent=False)),
     path('admin/', admin.site.urls),
     path('api/v1/', include(('api.v1.router', 'api_v1'), namespace='v1')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
